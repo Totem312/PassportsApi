c385a1fbd088a286f1d49bf1e6da09895c73a9be
DESKTOP-480N9JS\user <dddddaaaarrr8@gmail.com>
2022-09-01 15:36:31 +0300

    Исправление настроек базы данных

--- WebApi/Services/DbService.cs
+++ WebApi/Services/DbService.cs
@@ -7,13 +7,14 @@ namespace WebApi.Services
         
         public static void AddServicesPostrges(this WebApplicationBuilder builder)
         {
-            string Sqlconnection = builder.Configuration.GetConnectionString("SqlConnection");
-            builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Sqlconnection));
+            string pgConnection = builder.Configuration.GetConnectionString("SqlConnection");
+            builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(pgConnection));
         }
         public static void AddServicesMsSql(this WebApplicationBuilder builder)
         {
-            string Pgconnection = builder.Configuration.GetConnectionString("PgConnection");
-            builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(Pgconnection));
+         
+            string Sqlconnection = builder.Configuration.GetConnectionString("SqlConnection");
+            builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Sqlconnection));
         }
     }
 }
